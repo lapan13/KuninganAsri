7b005b593e0e1eebe3d34b0eb600af9bfb822ae4
thelapan <ariirwndi13@gmail.com>
2019-10-18 20:10:17 +0700

    change cardview style, add transparent layer on image and about_act

--- app/src/main/java/com/ariirwandi13/kuninganasri/About.java
+++ app/src/main/java/com/ariirwandi13/kuninganasri/About.java
@@ -3,6 +3,7 @@ package com.ariirwandi13.kuninganasri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = findViewById(R.id.toolbar3);
+        toolbar.setTitle("About Me");
     }
 }
