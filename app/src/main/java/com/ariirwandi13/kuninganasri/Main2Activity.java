4cc4e7837b8e4ac04451498fceb16175971b90f3
thelapan <ariirwndi13@gmail.com>
2019-10-20 23:16:25 +0700

    delete slider image, fix apk

--- app/src/main/java/com/ariirwandi13/kuninganasri/Main2Activity.java
+++ app/src/main/java/com/ariirwandi13/kuninganasri/Main2Activity.java
@@ -1,9 +1,9 @@
 package com.ariirwandi13.kuninganasri;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +11,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.smarteist.autoimageslider.IndicatorAnimations;
-import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
-import com.smarteist.autoimageslider.SliderView;
 
 public class Main2Activity extends AppCompatActivity {
-
-    SliderView sliderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +26,6 @@ public class Main2Activity extends AppCompatActivity {
         collapsingToolbarLayout.setExpandedTitleColor(
                 ContextCompat.getColor(this, R.color.colorPrimary));
 
-        Toolbar toolbar = findViewById(R.id.toolbar2);
-        setSupportActionBar(toolbar);
-        setTitle("");
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
-
-        sliderView = findViewById(R.id.image_activity2);
-
-        final SliderAdapter adapter = new SliderAdapter(this);
-        adapter.setCount(5);
-
-        sliderView.setSliderAdapter(adapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimations.SLIDE);
-        sliderView.setIndicatorSelectedColor(Color.WHITE);
-        sliderView.setIndicatorUnselectedColor(Color.GRAY);
-
-        sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
-            @Override
-            public void onIndicatorClicked(int position) {
-                sliderView.setCurrentPagePosition(position);
-            }
-        });
-
         Intent intent = getIntent();
         WisataItem wisataItem = intent.getParcelableExtra("Example Item");
 
@@ -65,6 +34,9 @@ public class Main2Activity extends AppCompatActivity {
         String harga = wisataItem.getHarga();
         String alamat = wisataItem.getAlamat();
         String desc = wisataItem.getDesc();
+
+        ImageView imageView = findViewById(R.id.image_activity2);
+        imageView.setImageResource(imageRes);
 
         TextView tvNama = findViewById(R.id.tv_nama2);
         tvNama.setText(nama);
